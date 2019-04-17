16f72b2d0818ac783f28055b3b22dc03fdcf02ae
“BA <m13141314974@163.com>
2019-04-17 22:01:14 +0800

    s

--- router/teacher.js
+++ router/teacher.js
@@ -2,6 +2,6 @@ const router = require('koa-router')();
 const config = require('../config');
 const controller = require('../controller/teacher');
 
-router.get(`${config.urlPrefix}/get_labels`, controller.getLabels);
+router.get(`${config.urlPrefix}/labels`, controller.getLabels);
 
 module.exports = router;
